b25b29e440e2200ab737732dd2c6a58196f43bff
Kang soyeong <dacapo@hufs.ac.kr>
2022-01-18 22:55:37 +0900

    9주차 실습 2

---
+++ 9주차_실습_리스트에 대하여.py
@@ -0,0 +1,7 @@
+group = ['A', 'B', 'C', 'D', 'E', 'F']
+name = ['가영', '나은', '다희', '라율']
+
+#group[0], name[0,1], group[1,2,3], name[2], group[4,5], name[3]
+
+all_list = [group[0], name[0:2], group[1:4], name[2], group[4:6], name[3]]
+print(all_list)
