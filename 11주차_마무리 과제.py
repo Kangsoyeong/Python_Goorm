68ccf4cf623d22f9681d58207dfd93fbed5eac4f
Kang soyeong <dacapo@hufs.ac.kr>
2022-01-20 22:37:59 +0900

    11주차 마무리 과제

---
+++ 11주차_마무리 과제.py
@@ -0,0 +1,7 @@
+d = {'pencil':500, 'eraser':700, 'notebook':1500, 'eraser':700, 'scissors':3000, 'tape':2700, 'pencil':500, 'pencil':500, 'notebook':1500, 'tape':2700}
+
+d['white'] = 1900
+print(d.get('white'))
+
+d.keys()
+print(len(d))
